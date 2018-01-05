58c110680a47a865d7a1eedff7e70d8a78570b53
Zafar <mdabuzafar78@gmail.com>
2018-01-05 16:59:49 +0530

    Reformat Code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import SignUpForm from './Components/SignUpForm';
+import SignUpForm from './Components/SignUp/SignUpForm';
 
 ReactDOM.render(<SignUpForm />, document.getElementById('root'));
 registerServiceWorker();
